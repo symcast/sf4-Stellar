5bc7ad616190fdce78d10272df362e00a6f1480c
ninjatn <ha.ben.gamra@gmail.com>
2019-07-17 00:56:47 +0200

    10. Generating URLs

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -13,17 +13,17 @@ class ArticleController extends  AbstractController
 {
 
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
 
     public function homepage()
     {
 
-        return new Response('OMG! My first page already! WOOO!');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
